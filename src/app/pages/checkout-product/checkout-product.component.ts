794ac121b5e633c7f2b22f5da3f6e9a2b45a6c98
wahyupurna <wahyupurna12@gmail.com>
2023-12-26 05:29:55 +0800

    product

--- src/app/pages/checkout-product/checkout-product.component.ts
+++ src/app/pages/checkout-product/checkout-product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MidtransService } from 'src/app/_services/midtrans.service';
 import { ProductService } from 'src/app/_services/product.service';
+import { environments } from 'src/app/environments/environments';
 import { Product } from 'src/app/product';
 import Swal from 'sweetalert2';
 
@@ -15,10 +17,12 @@ export class CheckoutProductComponent implements OnInit {
   selectedDate: string;
   minDate: string;
   checkOutDataForm: FormGroup = new FormGroup({});
+  midtransScript: HTMLScriptElement;
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private midtransService: MidtransService
   ) {
     const today = new Date();
     this.minDate = today.toISOString().split('T')[0];
@@ -31,7 +35,8 @@ export class CheckoutProductComponent implements OnInit {
     return this.checkOutDataForm.get('amountPeople')!;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.midtransInit();
     this.getData();
 
     this.checkOutDataForm = new FormGroup({
@@ -57,34 +62,85 @@ export class CheckoutProductComponent implements OnInit {
     });
   }
 
-  processPayment() {
+  midtransInit() {
+    const clientKey = environments.MIDTRANS_CLIENT_KEY;
+    this.midtransScript = document.createElement('script');
+    this.midtransScript.src = 'https://app.sandbox.midtrans.com/snap/snap.js';
+    this.midtransScript.setAttribute('data-client-key', clientKey);
+    this.midtransScript.async = true;
+    document.body.appendChild(this.midtransScript);
+
+    return () => {
+      document.body.removeChild(this.midtransScript);
+    };
+  }
+
+  processPaymentDirect() {
     if (this.checkOutDataForm.valid && this.product && this.product.price) {
-      const dateValue = this.date.value;
       const amountPeopleValue = this.amountPeople.value;
       const totalPrice = +this.product.price * +amountPeopleValue;
 
-      Swal.fire({
-        icon: 'question',
-        title: 'Confirm your order before making payment..',
-        showCancelButton: true,
-        html: `
-          <p>Date entered: ${dateValue}</p>
-          <p>Amount of People: ${amountPeopleValue}</p>
-          <p>Total Price: RM ${totalPrice.toFixed(2)}</p>
-        `,
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // API Midtrans Here
+      const data = {
+        name: this.product?.name,
+        price: totalPrice,
+        quantity: amountPeopleValue,
+        order_id: ~~(Math.random() * 100) + 1,
+      };
 
-          Swal.fire(
-            'Redirecting..',
-            'You will be navigate to the Midtrans Payment Gateway to complete your order..',
-            'info'
-          );
-        } else {
-          Swal.fire('Okay, action canceled!', '', 'info');
-        }
+      this.midtransService.createPaymentToken(data).subscribe({
+        next: (token) => {
+          console.log(token);
+          // Still developing
+        },
+        error(err) {
+          console.error(err);
+        },
       });
     }
   }
+
+  // processPayment() {
+  //   if (this.checkOutDataForm.valid && this.product && this.product.price) {
+  //     const dateValue = this.date.value;
+  //     const amountPeopleValue = this.amountPeople.value;
+  //     const totalPrice = +this.product.price * +amountPeopleValue;
+
+  //     Swal.fire({
+  //       icon: 'question',
+  //       title: 'Confirm your order before making payment..',
+  //       showCancelButton: true,
+  //       html: `
+  //         <p>Schedule Date: ${dateValue}</p>
+  //         <p>Amount of People: ${amountPeopleValue}</p>
+  //         <p>Total Price: MYR ${totalPrice}</p>
+  //       `,
+  //     }).then((result) => {
+  //       if (result.isConfirmed) {
+  //         const data = {
+  //           name: this.product?.name,
+  //           price: totalPrice,
+  //           quantity: amountPeopleValue,
+  //           order_id: ~~(Math.random() * 100) + 1,
+  //         };
+
+  //         this.midtransService.createPaymentToken(data).subscribe({
+  //           next: (token) => {
+  //             console.log(token);
+  //           },
+  //           error(err) {
+  //             console.error(err);
+  //           },
+  //         });
+
+  //         // Swal.fire(
+  //         //   'Redirecting..',
+  //         //   'You will be navigate to the Midtrans Payment Gateway to complete your order..',
+  //         //   'info'
+  //         // );
+  //       } else {
+  //         Swal.fire('Okay, action canceled!', '', 'info');
+  //       }
+  //     });
+  //   }
+  // }
 }
--- src/app/product.ts
+++ src/app/product.ts
@@ -1,12 +1,14 @@
 export interface Product {
-  _id?: string,
+  _id?: string;
   merchant_id?: string;
   name?: string;
   description?: string;
   address?: string;
   state?: string;
   city?: string;
-  price?: string;
+  price?: number;
   rating?: string;
-  image?: string,
+  image?: string;
+  total_sold?: string;
+  total_purchase?: string;
 }
